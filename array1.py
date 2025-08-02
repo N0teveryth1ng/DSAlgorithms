015e5dfd0b22580b4deab92a841701e50af0ec56
S Das <dasw67352@gmail.com>
2025-08-03 01:36:26 +0530

    trying arrays

--- array1.py
+++ array1.py
@@ -1,6 +1,6 @@
 # global array
 arr = [1,2,3,5,6,7,8,4]
-
+# arr = [3,3,4,5,6,6,8,8,9]
 
 # Largest element in a array - - - - - - - - - >
 def largest_elem(arr):
@@ -13,7 +13,6 @@ def largest_elem(arr):
     print(large) 
     
 # largest_elem(arr)            
-
 
 
 # second largets element of an array     
@@ -31,10 +30,11 @@ def secs():
        elif i > slargest and i < largest:
            slargest = i
            
-   print("seconf - ",slargest)
+   print("second - ",slargest)
 
 # secs()                
 
+# check if an arr is sorted or not
 def check(arr):
    n = len(arr)
    for i in range(1,n):
@@ -45,5 +45,71 @@ def check(arr):
    print('sorted')        
    return True   
 
+# check(arr)
 
-check(arr)
+
+# sort duplicated vals from the arr
+def check_dups(arr):
+        
+    if not arr:
+        return 
+    
+    j = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            j += 1
+            arr[j] = arr[i] 
+    
+    return j + 1
+
+length = check_dups(arr)
+# print(arr[:length])
+
+# optimal approach but takes high space [not good for DSA]
+def remove(arr):
+    res = []
+    for i in arr:
+        if i not in res:
+            res.append(i)
+            
+    return res
+
+# print(remove(arr))
+
+
+# LEFT Rotate process by one place 
+
+def left_rot():
+
+  arr = [1,2,3,5,6,7,8,4]
+  n = len(arr)
+  temp = arr[0]
+  
+  for i in range(1, n):    
+      arr[i-1] = arr[i]  
+      
+  
+  arr[n-1] = temp
+  return arr
+
+
+# print(left_rot())
+
+
+def right_rot():
+     
+   arr = [2,3,4,4,7,7,8]
+   n = len(arr)
+   temp = arr[n-1]
+   
+   for i in range(n-2,-1,-1):
+       arr[i+1] = arr[i]
+   
+   arr[0] = temp
+   return arr
+
+print(right_rot()) 
+       
+       
+       
