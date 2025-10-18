9ff8087bcca36fad118af0c6106effcc6d439d97
S Das <dasw67352@gmail.com>
2025-10-18 12:22:33 +0530

    mid of last slod

--- LinkedList/linkdlist_medium4DLL.py
+++ LinkedList/linkdlist_medium4DLL.py
@@ -27,31 +27,32 @@ class Dlinkedlist:
         
         
     
-    # delete the occurences in a LL 
+    # delete the occurences in a DLL 
     def delete_occurence(self, key):
         
         if not self.head:
             return
         
+        temp = self.head
+        
         while temp:
-            next_node = self.head.next 
+            next_node = temp.next 
             
             # if head is val then delete
-            if self.head.val == key:
+            if temp.val == key:
+                # if the node to delete is head 
                 if temp == self.head:
-                   self.head = self.head.next  
+                   self.head = temp.next  
                    if self.head: 
-                      next_node = self.head
                       self.head.prev = None
             else:
-                temp = self.head
                 # connect it with the prevs nodes after skipping
                 if temp.prev:
                    temp.prev.next = temp.next
                 if temp.next:
                     temp.next.prev = temp.prev  
                 
-
+                
         temp = next_node
         
         
@@ -76,27 +77,58 @@ class Dlinkedlist:
         return store
     
     
-    # optimal approach 
+    # optimal approach  - - -  pairs of sum - - ( very similar to binary search )   
     def optimal_pairs_sum(self, target_sum):
         
         if not self.head:
             return []
         
-        left = self.head
+        temp = self.head
+        left = temp
+        
+        right = self.head
+        while right.next:
+            right = right.next 
+        
         store = []
         
-        while left:
-            right = left.next
-            if left.val + right.val == target_sum:
-                store.append(left.val, right.val)
-            if left.val + right.val < target_sum:
+        while left != right and left.prev != right:
+            
+            current_sum = left.val + right.val
+            
+            if current_sum == target_sum:
+                store.append((left.val, right.val))
+                left = left.next
                 right = right.prev
+            elif target_sum < current_sum:
+                right = right.prev  
             else:
                 left = left.next
                 
         return store        
     
     
+    # remove the duplicates from a sorted DLL
+    def remove_duplic(self):
+        
+        if not self.head:
+            return
+        
+        temp = self.head
+        while temp and temp.next:
+            if temp.val == temp.next.val:
+                next_node = temp.next.next  
+                temp.next = next_node
+                if next_node:
+                    next_node.prev = temp
+            else:
+                temp = temp.next  
+        
+        return temp
+    
+    
+    
+    # 
     
     
     # display the LL in terminal 
@@ -116,10 +148,13 @@ ll.append(5)
 ll.append(6)
 ll.append(9)
 ll.append(3)
+ll.append(3)
 ll.display_forward()
 
 
 # print("pairs of sum - ", ll.pairs_sum(6))      
+# print("optimal pairs sum - ", ll.optimal_pairs_sum(8))
+print(" remove duplicates in dll - ", ll.remove_duplic())
          
 ll.display_forward()
         
