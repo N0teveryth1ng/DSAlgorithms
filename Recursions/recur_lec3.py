a8935701e7c7935b600b2c88d8f3b51619aa4b0f
S Das <dasw67352@gmail.com>
2025-11-11 00:14:13 +0530

    n queens is hard to understanad

--- Recursions/recur_lec3.py
+++ Recursions/recur_lec3.py
@@ -28,8 +28,78 @@ def palindrome_part(s):
         
 # print(palindrome_part("aab"))
                     
-
                     
-                
+                    
+# word search
+def matrix_word(board, word):
+    
+    rows, cols = len(board), len(board[0])
+    
+    def dfs(r,c,i):
+        if i == len(word):
+            return True
+        
+        if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != word[i]:
+            return False
+        
+        temp = board[r][c]
+        board[r][c] = "#"
+        
+        found = ( dfs(r + 1, c, i+1) or
+                   dfs(r - 1, c, i+1) or
+                   dfs(r, c + 1, i+1) or
+                   dfs(r, c - 1, i+1)) 
+
+        board[r][c] = temp
+        return found
+    
+    for r in range(rows):
+        for c in range(cols):
+            if board[r][c] == word[0] and dfs(r,c,0):
+                return True    
+    
+    return False
+
+# test of the fucntion  
+board = [
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+# print(matrix_word(board, "ABCCED"))
         
         
+        
+# N - Queens [LC - 51]
+def n_queens(n):
+    
+    rows, cols = len(n), len(n[0])
+    
+    def dfs(r,c,i):
+        
+        if i == len(n):
+            return True
+        
+        if r > 0 or  c > 0 or r > len(rows) or c > len(cols):
+            return False
+        
+        found = ( dfs(r + 1, c, i+1) or
+                  dfs(r - 1, c, i+1) or
+                  dfs(r, c + 1, i+1) or
+                  dfs(r, c - 1, i+1))
+        
+        return found
+    
+    for r in range(rows):
+        for c in range(cols):
+            if [r][c] == n[0]:
+                return True
+            
+    return False
+
+    
+    
+ 
+         
+    
