3d07f7d0313c92617a7c09531bc63f5ef72bd84a
S Das <dasw67352@gmail.com>
2025-12-02 23:08:32 +0530

    pt2 of bit maipulation done

--- bit_manipulation/bit_manipul_lec2.py
+++ bit_manipulation/bit_manipul_lec2.py
@@ -3,6 +3,79 @@
 
 
 # count num of bit needed to flip convert from A to B
-def flip():
+
+# rough implementation - - 
+     
+    #  3 = 011 
+    #  4 = 100  
+    # it counts as 3 bits to be changed 
+
+# we can use XOR here
+def flip(start, goal):
     
+    res = 0
+    while start or goal:
+        if (start % 2) != (goal % 2):
+            res += 1
+            
+        start = start >> 1
+        goal = goal >> 1
+        
+    return res
+
+# print(flip(7,4))
     
+# 2nd approach - - - - - 
+def flip2(start, goal):
+    
+    res = 0
+    while start or goal:
+        if (start & 1) != (goal & 1):
+            res += 1
+            
+        start = start // 2
+        goal = goal // 2
+        
+    return res
+
+# print(flip2(7,4))
+    
+# 3rd approch - - -  - 
+def flip3(start, goal):
+    
+    res = 0
+    n = start ^ goal
+    while n:
+        res += n & 1
+        n = n >> 1
+        
+    return res
+
+# print(flip3(7,4)) 
+
+
+# 4th approach - - brian kernighan method
+def flip4(start, goal):
+    
+    res = 0
+    n = start ^ goal
+    while n > 0:
+            n &= (n-1)
+            res += 1
+                
+    return res
+# print(flip4(7,4))
+
+
+
+# single number
+def single_nums(nums):
+    
+    res = 0
+    for i in nums:
+        res ^= i
+    return res
+
+# print(single_nums([4,6,4]))
+
+
