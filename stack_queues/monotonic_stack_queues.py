8061724322dc89d329718d2dc6d7a2458c7f4a50
S Das <dasw67352@gmail.com>
2025-12-28 12:35:53 +0530

    LC - 42

--- stack_queues/monotonic_stack_queues.py
+++ stack_queues/monotonic_stack_queues.py
@@ -184,3 +184,103 @@ def next_small_elem(s):
 # print(next_small_elem([1,6,3,9]))
 
 
+def prev_small(s):
+    
+    stack = []
+    n = len(s)
+    res = [-1] * n
+    
+    for i in range(n):
+        while stack and stack[-1] >= s[i]:
+            stack.pop()
+            
+        if stack:
+            res[i] = stack[-1]
+        else:
+            res[i] = -1
+            
+        stack.append(s[i])
+        
+    return res 
+
+# print(prev_small([1, 6, 3, 9]))
+
+
+# no. of NGE to the right 
+def number_NGE(arr):
+    
+    n = len(arr)
+    res = [0] * n
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[j] > arr[i]:
+                res[i] += 1
+       
+    return res
+
+# print(number_NGE([3, 4, 2, 7, 5, 8, 10, 6])) 
+
+
+
+
+# trapping of rain water - find max on both left and right til point i [LC - 42]
+def rain_water(height): # brute force approach 
+    
+    n = len(height)
+    total = 0
+    
+    for i in range(n):
+        
+        left_max = 0
+        for j in range(i):
+            if height[j] > left_max:
+                left_max = height[j]
+                
+        right_max = 0
+        for j in range(i+1, n):
+            if height[j] > right_max:
+                right_max = height[j]
+                
+        water = min(left_max, right_max) - height[i]
+        
+        if water > 0:
+            total += water
+            
+    return total
+
+# print(rain_water([0,1,0,2,1,0,1,3,2,1,2,1]))
+            
+
+
+# trapping rain water - [LC - 42] 
+def rain_water_optimal(height):
+    
+        n = len(height)
+        total = 0
+        
+        if n == 0:
+            return 0
+        
+        left_max = [0] * n
+        left_max[0] = height[0]
+        for i in range(n):
+            left_max[i] = max(left_max[i-1], height[i])
+                
+        right_max = [0] * n
+        right_max[n-1] = height[n-1]
+        for i in reversed(range(n-1)):
+            right_max[i] = max(right_max[i+1], height[i])
+                
+        for i in range(n):
+            water = min(left_max[i], right_max[i]) - height[i]
+            if water > 0:
+                total += water
+            
+        return total
+    
+# print(rain_water_optimal([4,2,0,3,2,5]))
+
+
+
+
