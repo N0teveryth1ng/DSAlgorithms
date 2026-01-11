cf899468e45ec517c8dd255e5b028e5dcd8f01c0
S Das <dasw67352@gmail.com>
2026-01-11 21:37:58 +0530

    lru and lfy remains

--- stack_queues/implementation_probs.py
+++ stack_queues/implementation_probs.py
@@ -50,5 +50,119 @@ def sliding_optimal_max(arr, k):
 # print(sliding_optimal_max([1,3,-1,-3,5,3,6,7], 3))
 
 
-            
 
+# Plus one - [POTD] 
+def plus_one(arr):
+    
+    n = len(arr)
+    for i in range(n-1, -1, -1):
+        if arr[i] < 9:
+            arr[i] += 1
+            return arr  
+        else:
+            arr[i] = 0
+            
+    return [1] + arr
+
+# print(plus_one([1,2,3]))
+
+
+
+# online stock span - brute idea  
+def stock_span(arr):
+    
+    n = len(arr)
+    span = [1] * n
+    
+    for i in range(n):
+        cnt = 0
+        for j in range(i-1, -1, -1):
+            if arr[i] >= arr[j]:
+                cnt += 1
+            else:
+                break
+            
+        span[i] = cnt
+            
+    return span
+
+# print(stock_span([100 ,80 ,60 ,70 ,60 ,75 ,85])) 
+        
+        
+# stock span [optimal sol] - - single price list based 
+# 
+def optimal_sol(arr):
+    
+    n = len(arr)
+    res = [0] * n
+    stack = [] 
+    
+    for i in range(n):
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+            
+        if stack:
+            res[i] = i - stack[-1]
+        else:
+            res[i] = i + 1
+            
+        stack.append(i)
+            
+    return res
+
+# print(optimal_sol([100 ,80 ,60 ,70 ,60 ,75 ,85]))
+              
+              
+              
+# class order based to handle unexpected price lists
+class stock:
+    def __init__(self):
+        self.stack = []
+        self.day_index = 0
+    
+    def stock_price(self, arr):
+        
+        while self.stack and self.stack[-1][0] <= arr:
+              self.stack.pop()
+              
+        if self.stack:
+            span = self.day_index - self.stack[-1][0]
+        else:
+            span = self.day_index + 1
+            
+        self.stack.append((arr, self.day_index))
+        self.day_index += 1
+        
+        return span
+    
+spanner = stock()
+# print(spanner.stock_price(80)) 
+
+
+
+# stock spanning problem - - -  
+def stock_planner(arr):
+    
+    n = len(arr)
+    res = [-1] * n
+    stack = []
+    
+    for i in range(n):
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+            
+        if stack:
+            res[i] = i - stack[-1]
+        else:
+            res[i] = i + 1
+            
+        stack.append(i)
+        
+    return res 
+
+# print(stock_planner([100, 80, 60, 70, 60, 75, 85]))
+
+
+
+# 
+    
