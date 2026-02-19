d9797782b77a226bb12ca5251125bced7887e92e
S Das <dasw67352@gmail.com>
2026-02-19 13:08:11 +0530

    Binary trees: finshed the fist seried of binary trees. ascending to the nezt one

--- binary_trees/traversals.py
+++ binary_trees/traversals.py
@@ -21,8 +21,9 @@ F = TreeNode(10)
 A.left = B
 A.right = C
 B.left = D
-B.left = E
+B.right = E
 C.right = F
+
 
 # print(A)
 
@@ -66,4 +67,80 @@ def postorder(node):
 
 
 
+from collections import deque
 
+# level order travesal
+def level_order(node):
+    
+    q = deque()
+    q.append(node)
+    
+    while q:
+        node = q.popleft()
+        print(node)
+        
+        if node.left:
+            q.append(node.left)
+        
+        if node.right:
+            q.append(node.right)
+            
+# print(level_order(A))
+        
+
+# level order based on (leetcode format)
+def test(root):
+    
+    if not root:
+        return []
+    
+    res = []
+    
+    q = deque()
+    q.append(root)
+    
+    while q:
+        current = []
+        for _ in range(len(q)):
+            node = q.popleft()
+            current.append(node.val)
+            
+            if  node.left:
+                q.append(node.left)
+                
+            if node.right:
+                q.append(node.right)
+                
+        res.append(current)
+            
+    return res
+                
+
+
+
+# maximum depth in BT 
+def max_depth_BT(node):
+    
+    if not node:
+        return 0
+    
+    left = max_depth_BT(node.left)
+    right = max_depth_BT(node.right)
+    return 1 + max(left, right)
+
+# print(max_depth_BT(A))
+
+
+
+# check for balanced binary tree
+def balanced_binary(node):
+    
+    if not node:
+        return False
+    
+    while node.left < node.right:
+        left = balanced_binary(node.left)
+        right = balanced_binary(node.right) 
+        abs(height(left) - height(right)) <= 1 
+        
+        
