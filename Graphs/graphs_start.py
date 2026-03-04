093cae80512b316af37d2ef2cb5f841beaf0eae4
S Das <dasw67352@gmail.com>
2026-03-04 14:16:09 +0530

    Graphs: getting fooled by BFS

--- Graphs/graphs_start.py
+++ Graphs/graphs_start.py
@@ -110,6 +110,7 @@ def bfs_traverse(start):
     
     
     
+    
 # popping method by DFS
 def dfs_traversal(start):
     
@@ -133,3 +134,104 @@ def dfs_traversal(start):
 
 
 
+# number of isalnds - [lc - 200]
+def num_islands(grid):
+    
+    if not grid:
+        return 0 
+    
+    rows, cols = len(grid), len(grid[0])
+    islands = 0
+    
+    
+    def dfs(r, c):
+        if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+            return
+        grid[r][c] = '0'  
+        dfs(r+1, c)
+        dfs(r-1, c)
+        dfs(r, c+1)
+        dfs(r, c-1) 
+    
+    
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1":
+                islands += 1
+                dfs(r, c)
+                    
+    return islands                 
+    
+# print(num_islands([
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]))
+    
+    
+    
+
+# province of city - [lc - 547] 
+def province(isConnect):
+    
+    if not isConnect:
+        return 0
+    
+    n = len(isConnect)
+    visited = [False] * n
+    isprovince = 0
+    
+    def dfs(city):
+        visited[city] = True
+        for nei in range(n):
+            if isConnect[city][nei] == 1 and not visited[nei]:
+                dfs(nei)
+    
+    for city in range(n):
+        if not visited[city]:
+            isprovince += 1
+            dfs(city)                   
+                   
+    return isprovince
+
+# print(province([[1,1,0],[1,1,0],[0,0,1]]))
+
+
+
+
+from collections import deque
+# rotting oranges --- [lc - 994]
+def rotting_oranges(grid):
+    
+    if not grid:
+        return
+    
+    rows, cols = len(grid), len(grid[0])
+    fresh = 0
+    d = deque()
+        
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 2:
+                d.append((r, c))
+            elif grid[r][c] == 1:
+                fresh += 1
+                
+                
+    minutes = 0
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]                
+    
+    while d and fresh > 0:
+        for _ in range(len(d)):
+            r, c  = d.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    fresh -= 1
+                    d.append((nr, nc))
+        
+        minutes += 1
+        
+    return minutes if fresh == 0 else -1
